798942465161915e0fa9216100c4c96f5dd75633
deyvid <dalbem88@gmail.com>
2019-04-27 11:27:53 -0400

    estilizacao do telefone_list.html e cadastroEndereco.html

--- gmSite/gmSite/urls.py
+++ gmSite/gmSite/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     #path('', TemplateView.as_view(template_name='home.html'), name="home"),
     path('', TemplateView.as_view(template_name='testeHome.html'), name="testeHome"),
-    path('teste/', TemplateView.as_view(template_name='testeHome.html'), name="testeHome"),
+    path('index/', TemplateView.as_view(template_name='testeHome.html'), name="testeHome"),
 ]
